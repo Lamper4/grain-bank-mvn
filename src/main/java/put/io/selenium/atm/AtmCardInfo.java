5ad5dde54c025b31006ba608909e73aa568df793
Lamper4 <kamil.lamperski1000@gmail.com>
2026-03-23 13:41:48 +0100

    revert pin change

--- src/main/java/put/io/selenium/atm/AtmCardInfo.java
+++ src/main/java/put/io/selenium/atm/AtmCardInfo.java
@@ -8,8 +8,8 @@ package put.io.selenium.atm;
  */
 public class AtmCardInfo {
 
-	// final int RIGHT_PIN = 1525;
-	final int RIGHT_PIN = 8008;
+	final int RIGHT_PIN = 1525;
+	// final int RIGHT_PIN = 8008;
 	
 	boolean isCardIn = false;
 	
